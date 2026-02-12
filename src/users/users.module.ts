5c698b6d5071104f3763f8f74f8087a52345ac20
ibrar11 <ibrarmunawar0gmail.com>
2026-02-12 15:14:40 +0500

    created controller for users and tweet modules

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,9 @@
 import { Module } from "@nestjs/common";
+import { UserController } from "./users.controller";
 
-@Module({})
+@Module({
+    controllers:[
+        UserController
+    ]
+})
 export class UserModule {}
