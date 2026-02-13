b74e0088508f2d56d3fa14180ec11646cfe2802d
ibrar11 <ibrarmunawar0gmail.com>
2026-02-13 18:47:34 +0500

    perform inter module dependency

--- src/tweet/tweet.module.ts
+++ src/tweet/tweet.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TweetController } from './tweet.controller';
 import { TweetService } from './tweet.service';
+import { UserModule } from 'src/users/users.module';
 
 @Module({
+  imports:[
+    UserModule
+  ],
   controllers: [TweetController],
   providers: [TweetService]
 })
--- src/tweet/tweet.service.ts
+++ src/tweet/tweet.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class TweetService {}
+export class TweetService {
+    constructor(private readonly userService: UsersService){}
+
+    tweets: {text: string, date: Date, userId: number}[] = [
+        {text: 'some text', date: new Date('2024-11-12'), userId: 1},
+        {text: 'some other text', date: new Date('2024-08-12'), userId: 2},
+        {text: 'some more text', date: new Date('2024-11-12'), userId: 1},
+    ]
+
+    getTweets(userId: number){
+        const user = this.userService.users.find(user => user.id === userId)
+        console.log("useruseruseruseruser",user)
+        const tweets =  this.tweets.filter(tweet => tweet.userId === userId)
+        console.log("tweetstweetstweets",tweets)
+        const response = tweets.map(tweet => {
+            return {...tweet, name: user?.name}
+        })
+        return response;
+    }
+}
