c3ef4db670effa8ad7919d2d957a76c9f5f142bd
ibrar11 <ibrarmunawar0gmail.com>
2026-02-13 15:35:58 +0500

    created tweet service

--- src/tweet/tweet.controller.ts
+++ src/tweet/tweet.controller.ts
@@ -1,4 +1,9 @@
 import { Controller } from '@nestjs/common';
+import { TweetService } from './tweet.service';
 
 @Controller('tweet')
-export class TweetController {}
+export class TweetController {
+    constructor(private tweetService: TweetService) {
+        
+    }
+}
